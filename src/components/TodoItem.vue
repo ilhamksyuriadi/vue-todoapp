48eb7f3ed74dd2251890cfcd50ca061e3ac98a38
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2019-09-08 15:00:07 +0700

    second commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="todo-item">
+        <h3>{{todo.title}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"]
+}
+</script>
+
+<style scoped>
+    .todo-item{
+        background: #f4f4f4;
+        border-bottom: solid 1px rgb(196, 196, 196);
+    }
+</style>
