9670aaa5a30166ee605f2e1643e8a6e3da65eba6
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2019-09-16 16:43:57 +0700

    add form

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form>
+            <input type="text" name="title" placeholder="add todo">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "AddTodo"
+}
+</script>
+
+<style scoped>
+    form {
+    display: flex;
+    }
+
+    input[type="text"] {
+    flex: 10;
+    padding: 5px;
+    }
+
+    input[type="submit"] {
+    flex: 2;
+    }
+
+    .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+    }
+    
+    .btn:hover {
+        background: #666;
+    }
+</style>
