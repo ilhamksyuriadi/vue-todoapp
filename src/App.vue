c47715cec03e06b133e80db3baa03c1db95f0cef
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2019-09-17 23:37:42 +0700

    modify add and delete

--- src/App.vue
+++ src/App.vue
@@ -26,10 +26,27 @@ export default {
   },
   methods:{
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(
+      // eslint-disable-next-line  
+      res => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err => 
+      // eslint-disable-next-line
+      console.log(err))
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo]
+      const { title, completed } = newTodo
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+      .then( res => this.todos = [...this.todos, res.data] )
+      .catch( err => 
+      // eslint-disable-next-line
+      console.log(err))
+
+      
     }
   },
   created() {
